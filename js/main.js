91a4b393bfac55bd4485afc13bf892542f4d4d94
Ammar Haddad <alhaddadam@gmail.com>
2024-05-10 16:37:12 -0400

    added back to top btn

--- js/main.js
+++ js/main.js
@@ -12,3 +12,21 @@ $('[data-toggle="counter-up"]').counterUp({
   delay: 5,
   time: 2000,
 });
+
+
+let mybutton = document.getElementById("btn-back-to-top");
+window.onscroll = function () {
+scrollFunction();
+};
+
+function scrollFunction() {
+if (
+document.body.scrollTop > 20 ||
+document.documentElement.scrollTop > 20
+) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+}
+mybutton.addEventListener("click", backToTop);
